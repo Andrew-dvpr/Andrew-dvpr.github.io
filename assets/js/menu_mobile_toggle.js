9b6d541a2c05b05e2c40ce954669dd18cec5acbe
andrey_ry6alk0 <andrey.ry6alk0@gmail.com>
2020-09-09 11:25:54 +0300

    preloader

--- assets/js/menu_mobile_toggle.js
+++ assets/js/menu_mobile_toggle.js
@@ -2,32 +2,31 @@
 //     document.body.classList.add('loaded');
 // }
 
-$(window).on('load', function () {
-$('.pre_loader').fadeOut().end().delay(200).fadeOut('slow');
-});
+window.onload = function () {
+	document.body.classList.add('loaded_hiding');
+	window.setTimeout(function () {
+		document.body.classList.add('loaded');
+		document.body.classList.remove('loaded_hiding');
+	}, 500);
+}
 
 
+// (function () {
 
+// 	const cropElement = document.querySelectorAll('.cutter'), // выбор элементов 
+// 		size = 515 // кол-во символов 
+// 	endCharacter = '      . . .'; // окончание 
 
+// 	cropElement.forEach(el => {
+// 		let text = el.innerHTML;
 
+// 		if (el.innerHTML.length > size) {
+// 			text = text.substr(0, size);
+// 			el.innerHTML = text + endCharacter;
+// 		}
+// 	});
 
-
-(function () {
-
-    const cropElement = document.querySelectorAll('.cutter'), // выбор элементов 
-          size = 515                                             // кол-во символов 
-          endCharacter = '      . . .';                                  // окончание 
-
-    cropElement.forEach(el => {
-        let text = el.innerHTML;
-
-        if (el.innerHTML.length > size) {
-            text = text.substr(0, size);
-            el.innerHTML = text + endCharacter;
-        }
-    });
-
-}());
+// }());
 
 
 
@@ -36,44 +35,25 @@ $('body').prepend('<a href="#" class="back-to-top" alt="back"><i class="fa fa-ar
 
 
 var amountScrolled = 20;
-$(window).scroll(function() {
-	if ( $(window).scrollTop() > amountScrolled ) {
+$(window).scroll(function () {
+	if ($(window).scrollTop() > amountScrolled) {
 		$('a.back-to-top').fadeIn('slow');
 		$('.hidden').fadeIn('slow');
 		// $('.lang').fadeOut('slow');
-		
-		
+
+
 	} else {
 		$('a.back-to-top').fadeOut('slow');
 		$('.hidden').fadeOut('slow');
 		// $('.lang').fadeIn('slow');
-		
+
 	}
 });
 
-$('a.back-to-top').click(function() {
+$('a.back-to-top').click(function () {
 	$('html, body').animate({
 		scrollTop: 0
 	}, 1000);
 	return false;
 });
-
-
-//     var $win = $(window),
-//     $fixed = $('.fixed'),
-//     limit = 20;
-
-// function tgl (state) {
-//     $fixed.toggleClass('hidden', state);
-// }
-
-// $win.on('scroll', function () {
-//     var top = $win.scrollTop();
-    
-//     if (top < limit) {
-//         tgl(true);
-//     } else {
-//         tgl(false);
-//     }
-// });
 
